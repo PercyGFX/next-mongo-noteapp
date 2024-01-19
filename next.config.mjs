db2d3a20af09c94901e96bd6f2b29b590d1ed842
PercyGFX <isurangabtk@gmail.com>
2024-01-20 01:30:02 +0530

    disable cache on vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
+const nextConfig = {
+  async headers() {
+    return [
+      // Disable Vercel Data Cache for all routes
+      {
+        source: "/(.*)",
+        headers: [{ key: "Cache-Control", value: "no-store" }],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
