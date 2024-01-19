1ab4862ea5742595b8e610e40d98d0cf3892d1fa
PercyGFX <isurangabtk@gmail.com>
2024-01-20 01:20:47 +0530

    vercel mongo issue

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function ConnectToMongoDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URL!);
+    await mongoose.connect(process.env.MONGODB_URI!);
     console.log("Connected successfully");
   } catch (e) {
     console.error("Error connecting to MongoDB:", e);
